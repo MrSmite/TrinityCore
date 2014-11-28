2adffe8abbf19c7fc099505789eded1a66c09ee1
MrSmite <MrSmite@users.noreply.github.com>
2014-11-28 18:02:01 -0500

    Update characters_create_creaturehonor_message.sql
    Add missing semicolon which prevented SQL import

--- sql/updates/characters/characters_create_creaturehonor_message.sql
+++ sql/updates/characters/characters_create_creaturehonor_message.sql
@@ -9,4 +9,4 @@ CREATE TABLE `creaturehonor_message` (
   `guid` int(10) unsigned NOT NULL COMMENT 'characters.guid',
   `message_sent` int(1) unsigned DEFAULT '0' COMMENT 'Set to 0 to force message resend at login. Useful after CONF change.',
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tracks CreatureHonor script messages sent to characters.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tracks CreatureHonor script messages sent to characters.';
